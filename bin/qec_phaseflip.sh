91e1527cf82acf64c79ea09a57997983b0fe38b0
John Hurst <john.b.hurst@gmail.com>
2024-08-24 09:21:53 +1000

    fix: Simplified image filenames.

--- bin/qec_phaseflip.sh
+++ bin/qec_phaseflip.sh
@@ -10,52 +10,52 @@ IMAGEDIR=${SCRIPTDIR}/../images/qec_phaseflip
 mkdir -p ${IMAGEDIR}
 
 echo "Input state: |0>, I, no error"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_0ket_I_none.png
+$SCRIPT --filename=$IMAGEDIR/0ket_I_none.png
 echo "Input state: |0>, I, flip bit 0"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_0ket_I_bit0.png --flip=0
+$SCRIPT --filename=$IMAGEDIR/0ket_I_phase0.png --flip=0
 echo "Input state: |0>, I, flip bit 1"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_0ket_I_bit1.png --flip=1
+$SCRIPT --filename=$IMAGEDIR/0ket_I_phase1.png --flip=1
 echo "Input state: |0>, I, flip bit 2"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_0ket_I_bit2.png --flip=2
+$SCRIPT --filename=$IMAGEDIR/0ket_I_phase2.png --flip=2
 echo "Input state: |0>, X, no error"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_0ket_X_none.png --unitaryop=X
+$SCRIPT --filename=$IMAGEDIR/0ket_X_none.png --unitaryop=X
 echo "Input state: |0>, X, flip bit 0"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_0ket_X_bit0.png --unitaryop=X --flip=0
+$SCRIPT --filename=$IMAGEDIR/0ket_X_phase0.png --unitaryop=X --flip=0
 echo "Input state: |0>, X, flip bit 1"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_0ket_X_bit1.png --unitaryop=X --flip=1
+$SCRIPT --filename=$IMAGEDIR/0ket_X_phase1.png --unitaryop=X --flip=1
 echo "Input state: |0>, X, flip bit 2"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_0ket_X_bit2.png --unitaryop=X --flip=2
+$SCRIPT --filename=$IMAGEDIR/0ket_X_phase2.png --unitaryop=X --flip=2
 
 echo "Input state: |1>, I, no error"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_1ket_I_none.png --theta=pi
+$SCRIPT --filename=$IMAGEDIR/1ket_I_none.png --theta=pi
 echo "Input state: |1>, I, flip bit 0"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_1ket_I_bit0.png --theta=pi --flip=0
+$SCRIPT --filename=$IMAGEDIR/1ket_I_phase0.png --theta=pi --flip=0
 echo "Input state: |1>, I, flip bit 1"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_1ket_I_bit1.png --theta=pi --flip=1
+$SCRIPT --filename=$IMAGEDIR/1ket_I_phase1.png --theta=pi --flip=1
 echo "Input state: |1>, I, flip bit 2"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_1ket_I_bit2.png --theta=pi --flip=2
+$SCRIPT --filename=$IMAGEDIR/1ket_I_phase2.png --theta=pi --flip=2
 echo "Input state: |1>, X, no error"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_1ket_X_none.png --theta=pi --unitaryop=X
+$SCRIPT --filename=$IMAGEDIR/1ket_X_none.png --theta=pi --unitaryop=X
 echo "Input state: |1>, X, flip bit 0"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_1ket_X_bit0.png --theta=pi --unitaryop=X --flip=0
+$SCRIPT --filename=$IMAGEDIR/1ket_X_phase0.png --theta=pi --unitaryop=X --flip=0
 echo "Input state: |1>, X, flip bit 1"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_1ket_X_bit1.png --theta=pi --unitaryop=X --flip=1
+$SCRIPT --filename=$IMAGEDIR/1ket_X_phase1.png --theta=pi --unitaryop=X --flip=1
 echo "Input state: |1>, X, flip bit 2"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_1ket_X_bit2.png --theta=pi --unitaryop=X --flip=2
+$SCRIPT --filename=$IMAGEDIR/1ket_X_phase2.png --theta=pi --unitaryop=X --flip=2
 
-echo "Input state: |1:2>, I, no error"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_12ket_I_none.png --theta=1.231
-echo "Input state: |1:2>, I, flip bit 0"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_12ket_I_bit0.png --theta=1.231 --flip=0
-echo "Input state: |1:2>, I, flip bit 1"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_12ket_I_bit1.png --theta=1.231 --flip=1
-echo "Input state: |1:2>, I, flip bit 2"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_12ket_I_bit2.png --theta=1.231 --flip=2
-echo "Input state: |1:2>, X, no error"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_12ket_X_none.png --theta=1.231 --unitaryop=X
-echo "Input state: |1:2>, X, flip bit 0"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_12ket_X_bit0.png --theta=1.231 --unitaryop=X --flip=0
-echo "Input state: |1:2>, X, flip bit 1"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_12ket_X_bit1.png --theta=1.231 --unitaryop=X --flip=1
-echo "Input state: |1:2>, X, flip bit 2"
-$SCRIPT --filename=$IMAGEDIR/phaseflip_12ket_X_bit2.png --theta=1.231 --unitaryop=X --flip=2
+echo "Input state: |33:66>, I, no error"
+$SCRIPT --filename=$IMAGEDIR/3366ket_I_none.png --theta="2*atan(sqrt(2))"
+echo "Input state: |33:66>, I, flip bit 0"
+$SCRIPT --filename=$IMAGEDIR/3366ket_I_phase0.png --theta="2*atan(sqrt(2))" --flip=0
+echo "Input state: |33:66>, I, flip bit 1"
+$SCRIPT --filename=$IMAGEDIR/3366ket_I_phase1.png --theta="2*atan(sqrt(2))" --flip=1
+echo "Input state: |33:66>, I, flip bit 2"
+$SCRIPT --filename=$IMAGEDIR/3366ket_I_phase2.png --theta="2*atan(sqrt(2))" --flip=2
+echo "Input state: |33:66>, X, no error"
+$SCRIPT --filename=$IMAGEDIR/3366ket_X_none.png --theta="2*atan(sqrt(2))" --unitaryop=X
+echo "Input state: |33:66>, X, flip bit 0"
+$SCRIPT --filename=$IMAGEDIR/3366ket_X_phase0.png --theta="2*atan(sqrt(2))" --unitaryop=X --flip=0
+echo "Input state: |33:66>, X, flip bit 1"
+$SCRIPT --filename=$IMAGEDIR/3366ket_X_phase1.png --theta="2*atan(sqrt(2))" --unitaryop=X --flip=1
+echo "Input state: |33:66>, X, flip bit 2"
+$SCRIPT --filename=$IMAGEDIR/3366ket_X_phase2.png --theta="2*atan(sqrt(2))" --unitaryop=X --flip=2
