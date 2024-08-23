d24358c938c4948c7471582542ac802a4b391e0d
John Hurst <john.b.hurst@gmail.com>
2024-08-24 09:20:56 +1000

    fix: Refinements:
    - default shots 1000 for easier interpretation of result ratios
    - circuit plots: no fold, no barriers.

--- bin/phys460.py
+++ bin/phys460.py
@@ -11,14 +11,14 @@ from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 def get_parser(description):
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("--provider", type=str, default="aer", help="Provider (aer, fake_manila, fake_kyoto, etc), (default aer)")
-    parser.add_argument("--shots", type=int, default=1024, help="Number of shots")
+    parser.add_argument("--shots", type=int, default=1000, help="Number of shots")
     parser.add_argument("--filename", type=str, help="Filename for circuit diagram")
     parser.add_argument("--isa-filename", type=str, help="Filename for circuit diagram after ISA")
     return parser
 
 def run_circuit(args, circuit):
     if args.filename:
-        circuit.draw(output="mpl", filename=args.filename)
+        circuit.draw(output="mpl", filename=args.filename, fold=-1, plot_barriers=False)
 
     if args.provider == 'aer':
         backend = AerSimulator()
@@ -31,7 +31,7 @@ def run_circuit(args, circuit):
     isa_circuit = pm.run(circuit)
 
     if args.isa_filename:
-        isa_circuit.draw(output="mpl", filename=args.isa_filename)
+        isa_circuit.draw(output="mpl", filename=args.isa_filename, fold=-1, plot_barriers=False)
 
     job = sampler.run([isa_circuit], shots=args.shots)
     return job.result()
